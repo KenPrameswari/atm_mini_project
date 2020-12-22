b9ab8d5baa1d0845a19557a26e827c6663f718c2
KenPrameswari <76463189+KenPrameswari@users.noreply.github.com>
2020-12-22 16:05:06 +0700

    Create atm_card.py

---
+++ atm_card.py
@@ -0,0 +1,9 @@
+class ATMCard:
+    def __init__(self, defaultPin, defaultBalance):
+        #superATMCard, self).__init__()
+        #self.arg = arg
+        self.defaultPin = defaultPin
+        self.defaultBalance = defaultBalance
+#ATMCard1 = ATMCard(1234,10000)
+#print('PIN Anda : ' + str(ATMCard1.defaultPin))
+#print('Saldo Anda : ' + str(ATMCard1.defaultBalance))
